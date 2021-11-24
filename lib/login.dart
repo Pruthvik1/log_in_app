66994da3baf6215807c84c65c7646bd05c78dfa0
Pruthvik1 <parthlunagariya3938@gmail.com>
2021-11-24 18:10:46 +0530

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key? key}) : super(key: key);
+  @override
+  _MyLoginState createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext contex) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Simple Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+//MaterialApp(
+//debugShowCheckedModeBanner: false,
+//initialRoute:'login',
+//routes:{
+//'login':(contex)=>MyLogin()
+//},
+//)
